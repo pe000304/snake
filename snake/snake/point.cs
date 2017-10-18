dfa8a853965c5a1b52c8fef04e4f32ec418565fd
pe000304 <bsg887@gmail.com>
2017-10-18 22:17:53 +0300

    Конструктор+инкапсуляция

--- snake/snake/point.cs
+++ snake/snake/point.cs
@@ -12,6 +12,19 @@ namespace snake
         public int y;
         public char sym;
 
+        public point()
+        {
+            
+        }
+
+        public point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
